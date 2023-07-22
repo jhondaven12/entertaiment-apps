cf005b54a6c47dbd5d65e92cd35ab9ff0c67f570
jhondaven12 <78583928+jhondaven12@users.noreply.github.com>
2023-07-22 20:57:42 +0800

    Update Navigation.js

--- components/Navigation.js
+++ components/Navigation.js
@@ -28,7 +28,7 @@ function Navigation() {
                             <Link href="/">
                                 <div className={router.pathname === "/" ? 'activeLink' : ''}>
                                     <Image
-                                        src="/assets/icon-nav-home.svg"
+                                        src="assets/icon-nav-home.svg"
                                         alt="home-icon"
                                         fill={true}
                                     />
@@ -39,7 +39,7 @@ function Navigation() {
                             <Link href="/movie">
                                 <div className={router.pathname === "/movie" ? 'activeLink' : ''}>
                                     <Image
-                                        src="/assets/icon-nav-movies.svg"
+                                        src="assets/icon-nav-movies.svg"
                                         alt="movie-icon"
                                         fill={true}
                                     />
@@ -50,7 +50,7 @@ function Navigation() {
                             <Link href="/series">
                                 <div className={router.pathname === "/series" ? 'activeLink' : ''}>
                                     <Image
-                                        src="/assets/icon-nav-tv-series.svg"
+                                        src="assets/icon-nav-tv-series.svg"
                                         alt="movie-icon"
                                         fill={true}
                                     />
@@ -62,7 +62,7 @@ function Navigation() {
                             <Link href="/bookmark">
                                 <div className={router.pathname === "/bookmark" ? 'activeLink' : ''}>
                                     <Image
-                                        src="/assets/icon-nav-bookmark.svg"
+                                        src="assets/icon-nav-bookmark.svg"
                                         alt="movie-icon"
                                         fill={true}
                                     />
@@ -81,7 +81,7 @@ function Navigation() {
                                 }}>
                                 <div className="user-profile">
                                     <Image
-                                        src="/assets/image-avatar.png"
+                                        src="assets/image-avatar.png"
                                         alt="Profile Picture"
                                         fill={true}
                                         sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
